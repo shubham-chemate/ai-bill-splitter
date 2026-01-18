6ef061a79eedb04b4f2327f50cc4e38269412d9d
Shubham Chemate <shubhamchemate3@gmail.com>
2026-01-18 13:00:20 +0000

    using embed for prompt files

--- prompt-reading.go
+++ prompt-reading.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"embed"
 	"log/slog"
 	"os"
 )
 
+//go:embed prompts/*.txt
+var promptFS embed.FS
+
 func getBillReceptPrompt() []byte {
-	promptFileName := "./prompts/bill-prompt.txt"
-	prompt, err := os.ReadFile(promptFileName)
+	promptFileName := "prompts/bill-prompt.txt"
+	prompt, err := promptFS.ReadFile(promptFileName)
 	if err != nil {
 		slog.Error("failed to read bill prompt file", "error", err)
 		os.Exit(1)
@@ -16,8 +20,8 @@ func getBillReceptPrompt() []byte {
 }
 
 func readSplitRulesPrompt() []byte {
-	splitRulesFileName := "./prompts/rules-prompt.txt"
-	splitRulesPromptBytes, err := os.ReadFile(splitRulesFileName)
+	splitRulesFileName := "prompts/rules-prompt.txt"
+	splitRulesPromptBytes, err := promptFS.ReadFile(splitRulesFileName)
 	if err != nil {
 		slog.Error("failed to read split rules file", "error", err)
 		os.Exit(1)
